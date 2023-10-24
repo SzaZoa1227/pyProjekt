7b76fa036a66c8c278f3d9fb93c02458ef150612
SzaZoa1227 <szabozoard27@gmail.com>
2023-10-24 23:46:13 +0200

    4. pont megvan
    funkcióba raktam a dobókockám(egyszerűbb volt mint gondoltam), paraméterezhetővé tettem
    
    megcsináltam, hogy miután egy másik modulból hívok funkciót visszatérjen a menübe

--- __projekt.py
+++ __projekt.py
@@ -1,19 +1,25 @@
 import os
 import math
+from dobokocka import dobokocka
+import threading
+
 megyaprogram = True
+
 def pit():
     a = float(input("Add meg az egyik befogó hosszát!   "))
     b = float(input("Add meg a másik befogó hosszát!   "))
     print(f"{math.sqrt(a**2+b**2)} az átfogó hossza")
     input()
-    main()
+
 def negyzet():
     hatvanyalap = int(input("Add meg a hatványalapot!   "))
     print(hatvanyalap**2)
     input()
-    main()
+
 def programleall():
+    global megyaprogram
     megyaprogram = False
+
 def alapmuveletek():
     a = int(input("Add meg az első számot amivel elvégezzük a műveleteket:  "))
     b = int(input("Add meg a második számot amivel elvégezzük a műveleteket:    "))
@@ -23,18 +29,32 @@ def alapmuveletek():
     hanyados = a/b
     print(f"Összegük: {osszeg}\nKülönbségük: {kulonbseg}\nSzorzatuk: {szorzat}\nHányadosuk: {hanyados}")
     input()
-    main()
+
 def main():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(f"Az irányítások\n0: Kilépés a programból\n1: Négy alapművelet\n2: Megadott szám négyzete\n3: Pitagorasz tétel alkalmazása\n4: Dobókocka")
-    kezdes = int(input("Kérlek add meg a választott programot:  "))
-    if kezdes == 0:
-        programleall()
-    if kezdes == 1:
-        alapmuveletek()
-    if kezdes == 2:
-        negyzet()
-    if kezdes == 3:
-        pit()
+    while megyaprogram:
+        print(f"Az irányítások\n0: Kilépés a programból\n1: Négy alapművelet\n2: Megadott szám négyzete\n3: Pitagorasz tétel alkalmazása\n4: Dobókocka")
+        kezdes = int(input("Kérlek add meg a választott programot:  "))
+        if kezdes == 0:
+            programleall()
+        if kezdes == 1:
+            alapmuveletek()
+        if kezdes == 2:
+            negyzet()
+        if kezdes == 3:
+            pit()
+        if kezdes == 4:
+            nemjo = True
+            while nemjo:
+                try:
+                    ablakMeret = int(input("Kérlek adj meg egy ablakméretet!    "))
+                    nemjo = False
+                except ValueError:
+                    print("Egész számot adj meg!")
+            thread = threading.Thread(target=dobokocka, args=(ablakMeret,)) #NEM TUDJA KEZELNI HA SÍMÁN INT ÉRTÉKET ADOK NEKI, LISTA KELL VAGY TUPLE MI EZ AFÉDJÉFKLJADSFÉLJDASÉFLKDJAFÉLKADSFJÉLKDSAFJÉADS
+            thread.start()
+            thread.join()
 
-main()
+if __name__ == "__main__": #ha a jelenleg futtatott script nem import akkor folytatja míg 0-t nem nyomunk
+    megyaprogram = True
+    main()
--- dobokocka.py
+++ dobokocka.py
@@ -2,55 +2,54 @@ import pygame
 import random
 import sys
 import os
+def dobokocka(ablakMeret):
+    os.system('cls' if os.name == 'nt' else 'clear') #oprendszer alapján cleareli le a cli-t
+    pygame.init()
 
-os.system('cls' if os.name == 'nt' else 'clear') #oprendszer alapján cleareli le a cli-t
-ablakMeret = 600
-pygame.init()
+    FEHER = (255, 255, 255)
+    FEKETE = (0, 0, 0)
 
-FEHER = (255, 255, 255)
-FEKETE = (0, 0, 0)
+    ablak = pygame.display.set_mode((ablakMeret, ablakMeret))
+    pygame.display.set_caption("Dobókocka")
 
-ablak = pygame.display.set_mode((ablakMeret, ablakMeret))
-pygame.display.set_caption("Dobókocka")
-
-pottySugar = ablakMeret/20
-pottyKoordinatak = {            #dictionaryben a kocka értéke baloldalt és hozzátartozó kirajzolandó pöttyök koordinátája
-    1: [(ablakMeret/2, ablakMeret/2)],
-    2: [(ablakMeret/4, ablakMeret/4), (ablakMeret*3/4, ablakMeret*3/4)],
-    3: [(ablakMeret/4, ablakMeret/4), (ablakMeret/2, ablakMeret/2), (ablakMeret*3/4, ablakMeret*3/4)],
-    4: [(ablakMeret/4, ablakMeret/4), (ablakMeret/4, ablakMeret*3/4), (ablakMeret*3/4, ablakMeret/4), (ablakMeret*3/4, ablakMeret*3/4)],
-    5: [(ablakMeret/4, ablakMeret/4), (ablakMeret/4, ablakMeret*3/4), (ablakMeret/2, ablakMeret/2), (ablakMeret*3/4, ablakMeret/4), (ablakMeret*3/4, ablakMeret*3/4)],
-    6: [(ablakMeret/4, ablakMeret/4), (ablakMeret/4, ablakMeret/2), (ablakMeret/4, ablakMeret*3/4), (ablakMeret*3/4, ablakMeret/4), (ablakMeret*3/4, ablakMeret/2), (ablakMeret*3/4, ablakMeret*3/4)]
-}
-dobasSzam = 0
-kockaErtek = None
-stilus = pygame.font.Font(None, 24)
-szoveg = stilus.render('Nyomd meg a szóközt egy dobáshoz, vagy a "q"-t hogy kilépj', True, FEKETE)
-fut = True
-while fut:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: #ha x-et nyomsz az ablakon akkor is kilép
-            fut = False
-        if event.type == pygame.KEYDOWN: #ha egy event tipusa billentyuzet lenyomas
-            if event.key == pygame.K_SPACE: #ha ez az event szokoz lenyomas
-                dobasSzam += 1
-                kockaErtek = random.randint(1, 6)
-            elif event.key == pygame.K_q: #ha ez az event q betu lenyomasa
+    pottySugar = ablakMeret/20
+    pottyKoordinatak = {            #dictionaryben a kocka értéke baloldalt és hozzátartozó kirajzolandó pöttyök koordinátája
+        1: [(ablakMeret/2, ablakMeret/2)],
+        2: [(ablakMeret/4, ablakMeret/4), (ablakMeret*3/4, ablakMeret*3/4)],
+        3: [(ablakMeret/4, ablakMeret/4), (ablakMeret/2, ablakMeret/2), (ablakMeret*3/4, ablakMeret*3/4)],
+        4: [(ablakMeret/4, ablakMeret/4), (ablakMeret/4, ablakMeret*3/4), (ablakMeret*3/4, ablakMeret/4), (ablakMeret*3/4, ablakMeret*3/4)],
+        5: [(ablakMeret/4, ablakMeret/4), (ablakMeret/4, ablakMeret*3/4), (ablakMeret/2, ablakMeret/2), (ablakMeret*3/4, ablakMeret/4), (ablakMeret*3/4, ablakMeret*3/4)],
+        6: [(ablakMeret/4, ablakMeret/4), (ablakMeret/4, ablakMeret/2), (ablakMeret/4, ablakMeret*3/4), (ablakMeret*3/4, ablakMeret/4), (ablakMeret*3/4, ablakMeret/2), (ablakMeret*3/4, ablakMeret*3/4)]
+    }
+    dobasSzam = 0
+    kockaErtek = None
+    stilus = pygame.font.Font(None, 24) #font({betustilus}, {betumeret})
+    szoveg = stilus.render('Nyomd meg a szóközt egy dobáshoz, vagy a "q"-t hogy kilépj', True, FEKETE) #render({szoveg}, {antialiasing (boolean)}, {szin})
+    fut = True
+    while fut:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: #ha x-et nyomsz az ablakon akkor is kilép
                 fut = False
+            if event.type == pygame.KEYDOWN: #ha egy event tipusa billentyuzet lenyomas
+                if event.key == pygame.K_SPACE: #ha ez az event szokoz lenyomas
+                    dobasSzam += 1
+                    kockaErtek = random.randint(1, 6)
+                elif event.key == pygame.K_q: #ha ez az event q betu lenyomasa
+                    fut = False
 
-    ablak.fill(FEHER)
-    dobasErtekSzoveg = stilus.render(f"Kockád értéke:{kockaErtek}",True,FEKETE)
-    dobasSzamSzoveg = stilus.render(f"Dobásaid száma:{dobasSzam}",True,FEKETE)
+        ablak.fill(FEHER)
+        dobasErtekSzoveg = stilus.render(f"Kockád értéke:{kockaErtek}",True,FEKETE)
+        dobasSzamSzoveg = stilus.render(f"Dobásaid száma:{dobasSzam}",True,FEKETE)
 
-    if kockaErtek is not None:
-        for pottyHelye in pottyKoordinatak[kockaErtek]:
-            pygame.draw.circle(ablak, FEKETE, pottyHelye, pottySugar)
-            ablak.blit(dobasSzamSzoveg,(50,10))
-            ablak.blit(dobasErtekSzoveg, (50,30))
-    else:
-        ablak.blit(szoveg, (50, 10))
+        if kockaErtek is not None:
+            for pottyHelye in pottyKoordinatak[kockaErtek]:
+                pygame.draw.circle(ablak, FEKETE, pottyHelye, pottySugar)
+                ablak.blit(dobasSzamSzoveg,(50,10))
+                ablak.blit(dobasErtekSzoveg, (50,30))
+        else:
+            ablak.blit(szoveg, (50, 10))
 
-    pygame.display.flip()
+        pygame.display.flip()
 
-pygame.quit()
-sys.exit()
+    pygame.quit()
+    sys.exit()
