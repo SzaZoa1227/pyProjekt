ae3b86475551ca7252161b1c7b40a78bfdd15950
SzaZoa1227 <szabozoard27@gmail.com>
2023-11-05 11:03:33 +0100

    szépítések

--- __projekt.py
+++ __projekt.py
@@ -5,6 +5,7 @@ import threading
 from random import randint
 megyaprogram = True
 
+
 def pit():
     nemjo = 1
     while nemjo:
@@ -23,6 +24,7 @@ def pit():
     print(f"{math.sqrt(a**2+b**2)} az átfogó hossza")
     input("\nÜss egy entert ha továbbmehetünk!\t")
 
+
 def negyzet():
     nemjo = 1
     while nemjo:
@@ -33,22 +35,26 @@ def negyzet():
     print(hatvanyalap**2)
     input("\nÜss egy entert ha továbbmehetünk!\t")
 
+
 def programleall():
     global megyaprogram
     megyaprogram = False
+
 
 def alapmuveletek():
     nemjo = 1
     while nemjo:
         try:
-            a = float(input("\nAdd meg az első számot amivel elvégezzük a műveleteket:  "))
+            a = float(
+                input("\nAdd meg az első számot amivel elvégezzük a műveleteket:  "))
             nemjo = 0
         except ValueError:
             print("Számot adj meg!")
     nemjo = 1
     while nemjo:
         try:
-            b = int(input("Add meg a második számot amivel elvégezzük a műveleteket:    "))
+            b = int(
+                input("Add meg a második számot amivel elvégezzük a műveleteket:    "))
             nemjo = 0
         except ValueError:
             print("Számot adj meg!")
@@ -56,15 +62,20 @@ def alapmuveletek():
     kulonbseg = a-b
     szorzat = a*b
     hanyados = a/b
-    print(f"\nÖsszegük: {osszeg}\nKülönbségük: {kulonbseg}\nSzorzatuk: {szorzat}\nHányadosuk: {hanyados}")
+    print(
+        f"\nÖsszegük: {osszeg}\nKülönbségük: {kulonbseg}\nSzorzatuk: {szorzat}\nHányadosuk: {hanyados}")
     input("\nÜss egy entert ha továbbmehetünk!\t")
+
+
 def randomSzamok():
     szamok = []
     for i in range(10):
-        szamok.append(randint(-500,500))
+        szamok.append(randint(-500, 500))
     print(f"\nA generált számok: {szamok}")
     print(f"Ezek közül a legkissebb: {min(szamok)}")
     input("\nÜss egy entert ha továbbmehetünk!\t")
+
+
 def main():
     while megyaprogram:
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -84,20 +95,23 @@ def main():
             negyzet()
         if kezdes == 3:
             pit()
+        if kezdes == 5:
+            randomSzamok()
         if kezdes == 4:
             nemjo = True
             while nemjo:
                 try:
-                    ablakMeret = int(input("Kérlek adj meg egy ablakméretet!    "))
+                    ablakMeret = int(
+                        input("Kérlek adj meg egy ablakméretet!    "))
                     nemjo = False
                 except ValueError:
                     print("Egész számot adj meg!")
-            thread = threading.Thread(target=dobokocka, args=(ablakMeret,)) #NEM TUDJA KEZELNI HA SÍMÁN INT ÉRTÉKET ADOK NEKI, LISTA KELL VAGY TUPLE MI EZ AFÉDJÉFKLJADSFÉLJDASÉFLKDJAFÉLKADSFJÉLKDSAFJÉADS
+            # NEM TUDJA KEZELNI HA SÍMÁN INT ÉRTÉKET ADOK NEKI, LISTA KELL VAGY TUPLE MI EZ AFÉDJÉFKLJADSFÉLJDASÉFLKDJAFÉLKADSFJÉLKDSAFJÉADS
+            thread = threading.Thread(target=dobokocka, args=(ablakMeret,))
             thread.start()
             thread.join()
-        if kezdes == 5:
-            randomSzamok()
 
-if __name__ == "__main__": #ha a jelenleg futtatott script nem import akkor folytatja míg 0-t nem nyomunk
+
+if __name__ == "__main__":  # ha a jelenleg futtatott script nem import akkor folytatja míg 0-t nem nyomunk
     megyaprogram = True
     main()
