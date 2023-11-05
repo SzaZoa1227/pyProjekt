444225f941fe3ef4e8c7215aca4acab15ec7de46
SzaZoa1227 <szabozoard27@gmail.com>
2023-11-05 13:40:45 +0100

    tovabbi javitasok, nehany plusz megoldas

--- __projekt.py
+++ __projekt.py
@@ -1,117 +1,113 @@
 import os
 import math
-from dobokocka import dobokocka
-import threading
 from random import randint
-megyaprogram = True
+import threading
+from dobokocka import dobokocka
 
 
-def pit():
-    nemjo = 1
-    while nemjo:
+def pit():  # pitagorasz tetel alkalmazasa
+    rossz = True
+    while rossz:
         try:
-            a = float(input("\nAdd meg az egyik befogó hosszát!   "))
-            nemjo = 0
+            a = float(input("\nAdd meg az egyik befogó hosszát!\t"))
+            rossz = False
         except ValueError:
-            print("Számot adj meg!")
-    nemjo = 1
-    while nemjo:
+            print("\nSzámot adj meg!")
+            input("\nÜss egy entert ha továbbmehetünk!\t")
+    rossz = True
+    while rossz:
         try:
-            b = float(input("Add meg a másik befogó hosszát!   "))
-            nemjo = 0
+            b = float(input("\nAdd meg a második befogó hosszát!\t"))
+            rossz = False
         except ValueError:
-            print("Számot adj meg!")
+            print("\nSzámot adj meg!")
+            input("\nÜss egy entert ha továbbmehetünk!\t")
     print(f"{math.sqrt(a**2+b**2)} az átfogó hossza")
     input("\nÜss egy entert ha továbbmehetünk!\t")
 
 
-def negyzet():
-    nemjo = 1
-    while nemjo:
+def negyzet():  # negyzetre emeles
+    rossz = True
+    while rossz:
         try:
-            hatvanyalap = float(input("\nAdd meg a hatványalapot!   "))
+            hatvanyalap = float(input("\nAdd meg a hatványalapot!\t"))
+            rossz = False
         except ValueError:
-            print("Számot adj meg!")
+            print("\nSzámot adj meg!")
+            input("\nÜss egy entert ha továbbmehetünk!\t")
     print(hatvanyalap**2)
     input("\nÜss egy entert ha továbbmehetünk!\t")
 
 
-def programleall():
-    global megyaprogram
-    megyaprogram = False
-
-
-def alapmuveletek():
-    nemjo = 1
-    while nemjo:
+def alapmuveletek():  # alapmuveletek elvegzese
+    rossz = True
+    while rossz:
         try:
-            a = float(
-                input("\nAdd meg az első számot amivel elvégezzük a műveleteket:  "))
-            nemjo = 0
+            a = float(input("\nAdd meg az első számot:  "))
+            rossz = False
         except ValueError:
-            print("Számot adj meg!")
-    nemjo = 1
-    while nemjo:
+            print("\nSzámot adj meg!")
+            input("\nÜss egy entert ha továbbmehetünk!\t")
+    rossz = True
+    while rossz:
         try:
-            b = int(
-                input("Add meg a második számot amivel elvégezzük a műveleteket:    "))
-            nemjo = 0
+            b = float(input("\nAdd meg a második számot:  "))
+            rossz = False
         except ValueError:
-            print("Számot adj meg!")
-    osszeg = a+b
-    kulonbseg = a-b
-    szorzat = a*b
-    hanyados = a/b
+            print("\nSzámot adj meg!")
+            input("\nÜss egy entert ha továbbmehetünk!\t")
+    osszeg = a + b
+    kulonbseg = a - b
+    szorzat = a * b
+    hanyados = a / b
     print(
         f"\nÖsszegük: {osszeg}\nKülönbségük: {kulonbseg}\nSzorzatuk: {szorzat}\nHányadosuk: {hanyados}")
     input("\nÜss egy entert ha továbbmehetünk!\t")
 
 
-def randomSzamok():
-    szamok = []
-    for i in range(10):
-        szamok.append(randint(-500, 500))
+def randomSzamok():  # random szamok generalasa
+    # for loop a 10 random szam generalasara
+    szamok = [randint(-500, 500) for _ in range(10)]
     print(f"\nA generált számok: {szamok}")
     print(f"Ezek közül a legkissebb: {min(szamok)}")
     input("\nÜss egy entert ha továbbmehetünk!\t")
 
 
+def dobokockaFx():
+    ablakMeret = int(input("Kérlek adj meg egy ablakméretet!    "))
+    thread = threading.Thread(target=dobokocka, args=(ablakMeret,))
+    thread.start()
+    thread.join()
+
+
 def main():
-    while megyaprogram:
+    funkciok = {1: alapmuveletek,
+                2: negyzet,
+                3: pit,
+                4: dobokockaFx,
+                5: randomSzamok}
+    megy = True
+    while megy:
+        # mivel linuxon dolgozom, nekem is kell, hogy mukodjon a clear
         os.system('cls' if os.name == 'nt' else 'clear')
         print(f"Az irányítások\n0: Kilépés a programból\n1: Négy alapművelet\n2: Megadott szám négyzete\n3: Pitagorasz tétel alkalmazása\n4: Dobókocka\n5: 10 véletlen szám generálása -500 és 500, legkissebb kiválasztása")
-        nemjo = 1
-        while nemjo:
+        rossz = True
+        while rossz:
             try:
-                kezdes = int(input("Kérlek add meg a választott programot:  "))
-                nemjo = 0
+                kezdes = int(input("\nVálaszd ki mit szeretnél csinálni!:\t"))
+                rossz = False
             except ValueError:
-                print("A felsorolt lehetőségekből válassz!")
+                print("\nA megadott lehetőségek közül válassz!")
+                input("\nÜss egy entert ha továbbmehetünk!\t")
         if kezdes == 0:
-            programleall()
-        if kezdes == 1:
-            alapmuveletek()
-        if kezdes == 2:
-            negyzet()
-        if kezdes == 3:
-            pit()
-        if kezdes == 5:
-            randomSzamok()
-        if kezdes == 4:
-            nemjo = True
-            while nemjo:
-                try:
-                    ablakMeret = int(
-                        input("Kérlek adj meg egy ablakméretet!    "))
-                    nemjo = False
-                except ValueError:
-                    print("Egész számot adj meg!")
-            # NEM TUDJA KEZELNI HA SÍMÁN INT ÉRTÉKET ADOK NEKI, LISTA KELL VAGY TUPLE MI EZ AFÉDJÉFKLJADSFÉLJDASÉFLKDJAFÉLKADSFJÉLKDSAFJÉADS
-            thread = threading.Thread(target=dobokocka, args=(ablakMeret,))
-            thread.start()
-            thread.join()
+            megy = False
+        elif kezdes in funkciok:
+            fuggveny = funkciok[kezdes]
+            fuggveny()
+        else:
+            print("\nA megadott lehetőségek közül válassz!")
+            input("\nÜss egy entert ha továbbmehetünk!\t")
 
 
-if __name__ == "__main__":  # ha a jelenleg futtatott script nem import akkor folytatja míg 0-t nem nyomunk
-    megyaprogram = True
+if __name__ == "__main__":
     main()
