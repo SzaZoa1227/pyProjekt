b08a34be7dfa1748e26782ac5916aef3ac9a0535
SzaZoa1227 <szabozoard27@gmail.com>
2023-12-12 18:50:49 +0100

    projektfeladat 1. resz kesz

--- ujprojektfeladat/projekt.py
+++ ujprojektfeladat/projekt.py
@@ -17,7 +17,15 @@ def maximum(lista):
             index = i
     return index
 
-#minkereses,maxkereses, indexuk
+def SzomszedoSzamKulonbseg(lista):
+    legnagyobbKulonbseg = (lista[0],lista[1])
+    for i in range(1,len(lista)-1):
+        if lista[i] - lista[i+1] > legnagyobbKulonbseg[0] - legnagyobbKulonbseg[1]:
+            legnagyobbKulonbseg = (lista[i],lista[i+1])
+    return legnagyobbKulonbseg
+
 print(lista)
 print(f"\nA legkissebb szám a listában: {lista[minimum(lista)]}\nIndexe: {minimum(lista)}")
 print(f"\nA legnagyobb szám a listában: {lista[maximum(lista)]}\nIndexe: {maximum(lista)}")
+print("\nA nulla megtalálható a listában" if 0 in lista else "\nA nulla nem található meg a listában")
+print(f"A legnagyobb különbség két szomszédos elem közt: {SzomszedoSzamKulonbseg(lista)}")
