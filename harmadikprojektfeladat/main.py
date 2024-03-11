e05a4a1079a4aaeb588ebf902ac2a63c19c30d02
Zoard <szabozoard27@gmail.com>
2024-03-11 20:34:11 +0100

    3.feladat kesz

--- harmadikprojektfeladat/main.py
+++ harmadikprojektfeladat/main.py
@@ -3,9 +3,19 @@ def mpbe(bemenet:list) -> int:
     return bemenet[2] + bemenet[1]*60 + bemenet[0]*60*60
 #változók létrehozása
 nyersadatok = []
-
+bejovohivasok = {}
 #2. feladat
 with open("hivas.txt","rt",encoding="utf-8") as bemenet:
     for sor in bemenet:
         sor = sor.strip().split()
         nyersadatok.append(sor)
+
+#3. feladat
+for adat in nyersadatok:
+    if adat[0] not in bejovohivasok.keys():
+        bejovohivasok[adat[0]] = 1
+    else: bejovohivasok[adat[0]] += 1
+print("Harmadik feladat:")
+for ido, hivasok in bejovohivasok.items():
+    print(f"{ido} óra: {hivasok} hívás")
+    
