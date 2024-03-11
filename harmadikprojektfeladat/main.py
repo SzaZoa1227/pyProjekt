7c7fe83195f9956634b351ee2697139473cc6891
Zoard <szabozoard27@gmail.com>
2024-03-11 23:03:47 +0100

    nincs ötös feladat

--- harmadikprojektfeladat/main.py
+++ harmadikprojektfeladat/main.py
@@ -16,6 +16,14 @@ def mpbe(bemenet: list) -> int:
     return bemenet[2] + bemenet[1]*60 + bemenet[0]*60*60
 
 
+def rendesbe(masodpercbenIdopont: int) -> list:
+    ora = masodpercbenIdopont//3600
+    maradek = masodpercbenIdopont % 3600
+    perc = maradek//60
+    mp = maradek % 60
+    return [ora, perc, mp]
+
+
 def legk(hivasokhossza: list) -> int:
     legrovidebb = 0
     for i in range(1, len(hivasokhossza)):
@@ -25,11 +33,11 @@ def legk(hivasokhossza: list) -> int:
 
 
 # változók létrehozása
-nyersadatok = []
-hivasokhossza = []
-bejovohivasok = {}
-munkaidonBelul = []
-munkaidobenHossz = []
+nyersadatok: list = []
+hivasokhossza: list = []
+bejovohivasok: dict = {}
+munkaidonBelul: list = []
+munkaidobenHossz: list = []
 # 2. feladat
 with open("hivas.txt", "rt", encoding="utf-8") as bemenet:
     for sor in bemenet:
@@ -37,6 +45,7 @@ with open("hivas.txt", "rt", encoding="utf-8") as bemenet:
         for i in range(len(sor)):
             sor[i] = int(sor[i])
         nyersadatok.append(sor)
+    bemenet.close()
 # 3. feladat
 for adat in nyersadatok:
     if adat[0] not in bejovohivasok.keys():
@@ -65,10 +74,11 @@ for adat in munkaidonBelul:
     hossz = mpbe(ig) - mpbe(tol)
     munkaidobenHossz.append(hossz)
 print("\nÖtös feladat:\n")
-inputOra = bekeres("Adj meg egy munkaidőn belüli órát! ",
-                   8, 12, "Munkaidőn belüli órát adj meg!")
-inputPerc = bekeres("Adj meg az órához egy perc értéket! ",
-                    0, 59, "0-59 közötti értéket adj meg!")
-inputMp = bekeres("Adj meg az órához és a perchez egy másodperc értéket! ",
-                  0, 59, "0-59 közötti értéket adj meg!")
+inputOra: int = bekeres("Adj meg egy munkaidőn belüli órát! ",
+                        8, 12, "Munkaidőn belüli órát adj meg!")
+inputPerc: int = bekeres("Adj meg az órához egy perc értéket! ",
+                         0, 59, "0-59 közötti értéket adj meg!")
+inputMp: int = bekeres("Adj meg az órához és a perchez egy másodperc értéket! ",
+                       0, 59, "0-59 közötti értéket adj meg!")
 print(f"A választott időpontod: {inputOra}:{inputPerc}:{inputMp}")
+#nem csinalom meg az 5.feladatot, ez mialofasz?
