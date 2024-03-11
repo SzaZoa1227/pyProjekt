2ef82fe592eaa82e9b7e4cdabdf4904580b518db
Zoard <szabozoard27@gmail.com>
2024-03-11 20:30:44 +0100

    2.feladat kesz

--- harmadikprojektfeladat/main.py
+++ harmadikprojektfeladat/main.py
@@ -1,3 +1,11 @@
 # Projektfeladat
 def mpbe(bemenet:list) -> int:
-    return bemenet[2] + bemenet[1]*60 + bemenet[0]*60*60 
+    return bemenet[2] + bemenet[1]*60 + bemenet[0]*60*60
+#változók létrehozása
+nyersadatok = []
+
+#2. feladat
+with open("hivas.txt","rt",encoding="utf-8") as bemenet:
+    for sor in bemenet:
+        sor = sor.strip().split()
+        nyersadatok.append(sor)
