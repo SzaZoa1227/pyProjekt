438e6e36a965947b82cb62a7b81268c34da61857
SzaZoa1227 <116081320+SzaZoa1227@users.noreply.github.com>
2024-03-22 08:55:59 +0100

    Update main.py

--- harmadikprojektfeladat/main.py
+++ harmadikprojektfeladat/main.py
@@ -1,4 +1,3 @@
-# Projektfeladat
 def bekeres(rizsa: str, minimum: int, maximum: int, hiba: str) -> int:
     while True:
         try:
@@ -31,14 +30,11 @@ def legh(hivasokhossza: list) -> int:
             legh = i
     return hivasokhossza[legh], legh
 
-
-# változók létrehozása
 nyersadatok: list = []
 hivasokhossza: list = []
 bejovohivasok: dict = {}
 munkaidonBelul: list = []
 munkaidobenHossz: list = []
-# 2. feladat
 with open("hivas.txt", "rt", encoding="utf-8") as bemenet:
     for sor in bemenet:
         sor = sor.strip().split()
@@ -46,7 +42,6 @@ with open("hivas.txt", "rt", encoding="utf-8") as bemenet:
             sor[i] = int(sor[i])
         nyersadatok.append(sor)
     bemenet.close()
-# 3. feladat
 for adat in nyersadatok:
     if adat[0] not in bejovohivasok.keys():
         bejovohivasok[adat[0]] = 1
@@ -55,7 +50,6 @@ for adat in nyersadatok:
 print("\nHarmadik feladat:\n")
 for ido, hivasok in bejovohivasok.items():
     print(f"{ido} óra: {hivasok} hívás")
-# 4. feladat
 for adat in nyersadatok:
     tol = [adat[0], adat[1], adat[2]]
     ig = [adat[3], adat[4], adat[5]]
@@ -64,7 +58,6 @@ for adat in nyersadatok:
 print("\nNégyes feladat:\n")
 print(
     f"{legh(hivasokhossza)[0]} másodpercig tartott a leghosszabb hívás, melynek sorszáma: {legh(hivasokhossza)[1]}")
-# 5. feladat
 for i in range(len(nyersadatok)):
     if nyersadatok[i][0] >= 8 and nyersadatok[i][0] < 12:
         munkaidonBelul.append((nyersadatok[i], i))
@@ -93,10 +86,7 @@ for i in range(len(nyersadatok)):
         valasztottHivasokKorul.append((nyersadatok[i], i))
 print(
     f"A megadott időpontban: {inputOra}:{inputPerc}:{inputMp} {valasztottHivasokKorul[1][1]}. sorszámú telefonáló volt vonalban, ekkkor várt: {len(valasztottHivasokKorul)-1} telefonáló.")
-# 6. feladat
 print("nincs hatos feladat")
-#7. feladat
-# print(munkaidonBelul)
 with open("sikeres.txt","wt",encoding="utf-8") as ki:
     for i in range(len(munkaidonBelul)):
         szoveg: str = f"{i+1} {munkaidonBelul[i][0][0]} {munkaidonBelul[i][0][1]} {munkaidonBelul[i][0][2]} {munkaidonBelul[i][0][3]} {munkaidonBelul[i][0][4]} {munkaidonBelul[i][0][5]}\n"
